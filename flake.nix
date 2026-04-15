ad83cd2742c8435a25a15043053ea2e48c30c7a3
Evan Piro <evanpiro@Evans-MacBook-Pro.local>
2026-04-14 23:54:55 -0400

    files

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Haskell development environment with HLS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        haskell = pkgs.haskellPackages;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            ghc
+            cabal-install
+            haskell-language-server
+          ];
+        };
+      });
+}
